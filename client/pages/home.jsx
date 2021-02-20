c925a8c048dbec6688358f449e740ded04614eee
Boyul Kim <boyulkim1@gmail.com>
2021-02-20 15:48:30 -0800

    added chessboard

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Chessboard from 'chessboardjsx';
 
 export default function Home(props) {
   return (
     <>
-    <div>test</div>
+    <Chessboard
+      width={400}
+      position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    />
     </>
   );
 }
